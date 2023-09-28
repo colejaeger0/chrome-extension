de24811ac7741e5434413299807a7a1c7b77e687
Asher <momomitchellsmith@gmail.com>
2023-09-27 21:41:24 -0700

    chrome extension hackathon :')

---
+++ content.js
@@ -0,0 +1,17 @@
+document.querySelector('h1').innerText = 'AAAAAHHHHHHH';
+
+const imageLocations = document.querySelectorAll('img');
+
+imageLocations.forEach(image => {
+  image.setAttribute(
+    'src',
+    'https://media.licdn.com/dms/image/C4D03AQG2XwBr2rylkg/profile-displayphoto-shrink_800_800/0/1572454199260?e=2147483647&v=beta&t=3GltJtHfvBmYtLEntPM8Q12QK8_9K-jx_EeeMYQNuVg',
+  );
+});
+
+
+document.querySelector
+document.getElementById
+//NodeList
+
+//iterate over all the instances bc the outfit is an array
---
+++ phil.js
@@ -0,0 +1,16 @@
+console.log('test');
+
+const imageLoc = document.querySelectorAll('img');
+
+imageLoc.forEach(image => {
+  image.setAttribute(
+    'src',
+    'https://media.licdn.com/dms/image/C4D03AQG2XwBr2rylkg/profile-displayphoto-shrink_800_800/0/1572454199260?e=2147483647&v=beta&t=3GltJtHfvBmYtLEntPM8Q12QK8_9K-jx_EeeMYQNuVg',
+  );
+});
+
+document.querySelector;
+document.getElementById;
+//NodeList
+
+//iterate over all the instances bc the outfit is an array
